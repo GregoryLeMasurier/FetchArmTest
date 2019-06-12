5f791aa2f081db71507ee71fcbf54f3fa7cd00f4
gregory lemasurier <gregorylemasurier@gmail.com>
2019-06-12 15:10:56 -0400

    added new test file

--- src/arm_up.cpp
+++ src/arm_up.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
 
     std::vector<std::string> joints = {"torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
                        "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"};
-    std::vector<double> values = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    std::vector<double> values = {0.36, 1.57, 0, 0.0, -1.7, 0.0, -0.57, 0.0};
 
     std::map<std::string, double> variable_values;
     for (int i = 0; i < joints.size(); ++i) {
